2c3913cc44712004f0718517dbfa5c7339ad0642
Mathemagician123 <themathaces@gmail.com>
2023-01-31 19:47:22 -0800

    Setup

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="independent_set_gym",
     version="0.0.1",
-    install_requires=["gym==0.26.2"]
+    install_requires=["gym==0.26.2"] # change on update
 )
